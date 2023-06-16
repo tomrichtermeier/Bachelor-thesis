d6675947de5ceace4df877efaa1dc1b4fd0647f7
Tom Richtermeier <tom_richtermeier@eva.mpg.de>
2023-06-16 10:27:20 +0200

    Add script for downloading data

---
+++ 02-scripts/curl_download_script.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/033/SRR12557733/SRR12557733_1.fastq.gz -o SRR12557733_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/034/SRR12557734/SRR12557734_1.fastq.gz -o SRR12557734_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/006/SRR12557706/SRR12557706_2.fastq.gz -o SRR12557706_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/004/SRR12557704/SRR12557704_2.fastq.gz -o SRR12557704_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/033/SRR12557733/SRR12557733_2.fastq.gz -o SRR12557733_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/004/SRR12557704/SRR12557704_1.fastq.gz -o SRR12557704_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/007/SRR12557707/SRR12557707_2.fastq.gz -o SRR12557707_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/022/SRR12557722/SRR12557722_2.fastq.gz -o SRR12557722_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/011/SRR12557711/SRR12557711_2.fastq.gz -o SRR12557711_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/034/SRR12557734/SRR12557734_2.fastq.gz -o SRR12557734_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/005/SRR12557705/SRR12557705_2.fastq.gz -o SRR12557705_2.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/022/SRR12557722/SRR12557722_1.fastq.gz -o SRR12557722_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/007/SRR12557707/SRR12557707_1.fastq.gz -o SRR12557707_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/005/SRR12557705/SRR12557705_1.fastq.gz -o SRR12557705_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/006/SRR12557706/SRR12557706_1.fastq.gz -o SRR12557706_1.fastq.gz
+curl -L ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR125/011/SRR12557711/SRR12557711_1.fastq.gz -o SRR12557711_1.fastq.gz
